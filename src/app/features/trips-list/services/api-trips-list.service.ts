2d37871c0aac9ebd47e651765cab9940aa34cff0
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2024-12-04 01:45:11 +0100

    add alias path

--- src/app/features/trips-list/services/api-trips-list.service.ts
+++ src/app/features/trips-list/services/api-trips-list.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '@env/environment';
 import { Observable, tap } from 'rxjs';
 import { TripFilters, TripResponse } from '../../../shared/models/trips.models';
 import { HttpClient, HttpParams } from '@angular/common/http';
